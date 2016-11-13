b5d7878da191c96fa646d765231433b5880d286b
RX14 <chris@rx14.co.uk>
2016-11-13 22:43:01 +0000

    Fix extension parsing for multibyte characters

--- src/pomf/util/string.cr
+++ src/pomf/util/string.cr
@@ -16,4 +16,45 @@ class String
 
     slug.empty? ? "-" : slug
   end
+
+  # Monkeypatch to fix https://github.com/crystal-lang/crystal/issues/3513
+  # FIXME: remove after 0.20.0
+
+  def rindex(search : Char, offset = size - 1)
+    # If it's ASCII we can delegate to slice
+    if search.ascii? && ascii_only?
+      return to_slice.rindex(search.ord.to_u8, offset)
+    end
+
+    offset += size if offset < 0
+    return nil if offset < 0
+
+    last_index = nil
+
+    each_char_with_index do |char, i|
+      if i <= offset && char == search
+        last_index = i
+      end
+    end
+
+    last_index
+  end
+
+  def index(search : Char, offset = 0)
+    # If it's ASCII we can delegate to slice
+    if search.ascii? && ascii_only?
+      return to_slice.index(search.ord.to_u8, offset)
+    end
+
+    offset += size if offset < 0
+    return nil if offset < 0
+
+    each_char_with_index do |char, i|
+      if i >= offset && char == search
+        return i
+      end
+    end
+
+    nil
+  end
 end
