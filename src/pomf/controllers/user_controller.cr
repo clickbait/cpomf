5c5eaedd300f8c02ebecefd5534be87f1d889363
Neko <neko@users.noreply.github.com>
2016-11-14 15:14:27 +1030

    disregard case on email login

--- src/pomf/controllers/user_controller.cr
+++ src/pomf/controllers/user_controller.cr
@@ -17,7 +17,7 @@ module Pomf
       end
 
       if errors.empty?
-        user = Models::User.where("email = $1", [email])
+        user = Models::User.where("lower(email) = $1", [email.downcase])
 
         if user.nil?
           errors << "Email or Password does not match."
